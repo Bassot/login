dbc0e6e5c4a439b050a6a1ef24e91fb86786e9f7
BassoX <basso.andrea.96@gmail.com>
2023-03-13 16:45:08 +0100

    first home component in angular

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose = require("mongoose");
 var express = require("express");
 var user = require("./User");
+var bodyParser = require("body-parser");
+var colors = require("colors");
+colors.enabled = true;
+/* FILE .ENV */
+var dotenv = require('dotenv');
+//import dotenv from 'dotenv';
+dotenv.config();
+//console.log(process.env);
+if (dotenv.error) {
+    console.log("Unable to load \".env\" file. Please provide one to store the JWT secret key".red);
+    process.exit(-1);
+}
+else if (!process.env.JWT_SECRET) {
+    console.log("\".env\" file loaded but JWT_SECRET=<secret> key-value pair was not found".red);
+    process.exit(-1);
+}
 var app = express();
+//let auth = jwt.expressjwt({secret: process.env.TOKEN_SECRET});
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.status(200).json({ api_version: '1.0', author: 'BassHound' });
+});
+app.get('/show', function (req, res) {
+    res.send("Hello World!");
+});
+app.listen(3000);
 mongoose.connect('mongodb://localhost:27017/login2023').then(function () {
     console.log('Connected to MongoDB');
     return user.getModel().findOne({ username: 'BassHound' });
@@ -12,7 +37,7 @@ mongoose.connect('mongodb://localhost:27017/login2023').then(function () {
         console.log('The Admin User already exists');
         return;
     }
-    console.log('Creating the Admin User');
+    console.log('Creating the Admin User...');
     var model = user.getModel();
     var data = {
         username: 'BassHound',
@@ -23,12 +48,4 @@ mongoose.connect('mongodb://localhost:27017/login2023').then(function () {
     var andrea = new model(data);
     return andrea.save();
 });
-// JWT token...
-app.get('/', function (req, res) {
-    res.status(200).json({ api_version: '1.0', author: 'BassHound' });
-});
-app.get('/show', function (req, res) {
-    res.send("Hello World!");
-});
-app.listen(3000);
 //# sourceMappingURL=index.js.map
--- Server/index.ts
+++ Server/index.ts
@@ -1,9 +1,38 @@
 import * as mongoose from 'mongoose';
 import * as express from 'express';
 import * as user from './User';
-import * as colors from 'colors';
-
+import * as bodyParser from "body-parser";
+import jsonWebToken = require('jsonwebtoken');
+import * as jwt from 'express-jwt';
+import colors = require('colors');
+colors.enabled = true;
+/* FILE .ENV */
+const dotenv = require('dotenv');
+//import dotenv from 'dotenv';
+dotenv.config();
+//console.log(process.env);
+/*
+if (dotenv.error) {
+    console.log("Unable to load \".env\" file. Please provide one to store the JWT secret key".red);
+    process.exit(-1);
+} else if( !process.env.JWT_SECRET ) {
+    console.log("\".env\" file loaded but JWT_SECRET=<secret> key-value pair was not found".red);
+    process.exit(-1);
+}
+*/
 const app = express();
+//let auth = jwt.expressjwt({secret: process.env.JWT_SECRET});
+
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    res.status(200).json({api_version: '1.0', author: 'BassHound'});
+});
+app.get('/show', function (req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(3000);
+
 mongoose.connect('mongodb://localhost:27017/login2023').then(()=>{
     console.log('Connected to MongoDB');
     return user.getModel().findOne({username: 'BassHound'});
@@ -12,7 +41,7 @@ mongoose.connect('mongodb://localhost:27017/login2023').then(()=>{
         console.log('The Admin User already exists');
         return ;
     }
-    console.log('Creating the Admin User');
+    console.log('Creating the Admin User...');
     let model = user.getModel();
     let data = {
         username: 'BassHound',
@@ -23,15 +52,3 @@ mongoose.connect('mongodb://localhost:27017/login2023').then(()=>{
     let andrea = new model(data);
     return andrea.save();
 });
-
-// JWT token...
-
-app.get('/', function (req, res) {
-    res.status(200).json({api_version: '1.0', author: 'BassHound'});
-});
-app.get('/show', function (req, res) {
-    res.send("Hello World!");
-});
-
-app.listen(3000);
-
